884f80abbf9d76ef915e73de9468c271303bf758
juan13ju <juan13ubaldo@gmail.com>
2022-04-01 14:01:06 -0600

    Tarea 2

---
+++ Tarea2/app/src/main/java/fciencias/pdm2022/tarea2/MainActivity.java
@@ -0,0 +1,27 @@
+package fciencias.pdm2022.tarea2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_enviar = findViewById(R.id.btn_enviar_txt);
+
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
